9eb79bfe47918465f6c6836fe388a0f00bae14cd
Florian <floriannchoh@gmail.com>
2020-05-09 20:52:10 +0200

    command menu deleted

--- inscription.py
+++ inscription.py
@@ -133,7 +133,7 @@ menu_bar = Menu(window)
 
 # creer un premier menu
 file_menu = Menu(menu_bar, tearoff=0)
-file_menu.add_command(label="Nouveau", command=pseudo)
+file_menu.add_command(label="Nouveau", command=initialisation)
 file_menu.add_command(label="Quitter", command=window.quit)
 menu_bar.add_cascade(label="Fichier", menu=file_menu)
 

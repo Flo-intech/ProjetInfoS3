92dc065601d6c072755575709a9e908c0ed3f079
Felipe <felipe.bonilla.1998@gmail.com>
2020-06-10 15:05:26 +0200

    barre menu

--- testprojet.py
+++ testprojet.py
@@ -8,11 +8,48 @@ On crée une fenêtre simple qui souhaite la bienvenue à l'utilisateur.
 from tkinter import *
 import tkinter as tk
 
+#menu
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
+
 # On crée une fenêtre, racine de notre interface
 fenetre = Tk()
 can1 = Canvas(fenetre, height=1700, width=1400)
 can1.pack()
+menubar = Menu(fenetre)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="FOOTBALL", command=donothing)
+filemenu.add_command(label="BASKETBALL", command=donothing)
+filemenu.add_command(label="TENNIS", command=donothing)
+filemenu.add_command(label="NATATION", command=donothing)
+filemenu.add_command(label="ATHLETISME", command=donothing)
 
+#menu
+filemenu.add_separator()
+
+filemenu.add_command(label="Exit", command=fenetre.quit)
+menubar.add_cascade(label="Menu", menu=filemenu)
+editmenu = Menu(menubar, tearoff=0)
+editmenu.add_command(label="Tout les sports", command=donothing)
+
+editmenu.add_separator()
+
+editmenu.add_command(label="Football", command=donothing)
+editmenu.add_command(label="Basketball", command=donothing)
+editmenu.add_command(label="Tennis", command=donothing)
+editmenu.add_command(label="Natation", command=donothing)
+editmenu.add_command(label="Athletisme", command=donothing)
+
+menubar.add_cascade(label="Quiz", menu=editmenu)
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Football", command=donothing)
+helpmenu.add_command(label="Basketball", command=donothing)
+helpmenu.add_command(label="Tennis", command=donothing)
+helpmenu.add_command(label="Natation", command=donothing)
+helpmenu.add_command(label="Athlétisme", command=donothing)
+menubar.add_cascade(label="Matchs en direct", menu=helpmenu)
 
 # fenetre
 fenetre.title("Mapcompetition")
@@ -34,4 +71,5 @@ can1.create_image(700, 400, image=mappemonde)
 
 
 # On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
+fenetre.config(menu=menubar)
 fenetre.mainloop()

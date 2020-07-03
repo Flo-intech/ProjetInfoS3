3d9db2b3e19b961cc884536c98cb09e018b3fb0e
Florian <floriannchoh@gmail.com>
2020-07-03 05:43:03 +0200

    change class Mappemonde - Carte

--- window.py
+++ window.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-from testprojet import Mappemonde
+from testprojet import Carte
 from sports import *
 from live import Bundesliga
 
@@ -163,7 +163,7 @@ class Connexion:
             self.pwd = pwd
             
             self.root3=Toplevel(self.master)
-            self.Map=Mappemonde(self.root3)
+            self.Map=Carte(self.root3)
       
 
         else:
